89016007f429931638e4b303cbe151cb6943fab9
unknown <khooyuxuan1227@gmail.com>
2026-08-14 13:53:56 +0800

    user/pass

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 define('APP_NAME', 'Parcel Delivery Management System');
 define('BASE_URL', '');
 define('APP_TIMEZONE', 'Asia/Kuala_Lumpur');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'synergy1_yuxuan_parcel_delivery');
 define('DB_USER', 'synergy1_yenping');
 define('DB_PASS', 'R.zb0ZwEuGZ}*fW2g');
